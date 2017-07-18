25d9899c3302d1d22f2ac9954a002857d5136a8a
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 17:12:13 +0000

    Added pry, created setportfolioitem, and fixed the technology file deletion error

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,10 +1,16 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   def index
     @portfolio_items = Portfolio.all
   end
   
   
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
@@ -25,12 +31,10 @@ class PortfoliosController < ApplicationController
   
   
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice:
@@ -43,34 +47,32 @@ class PortfoliosController < ApplicationController
   
   
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
   
   
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
+    # Destroy/delete the record's Tehcnologies
+    @portfolio_item.technologies.destroy
     # Destroy/delete the record
     @portfolio_item.destroy
-
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
   
-  def angular
-    @angular_portfolio_items = Portfolio.angular
-  end
 
   private
-  
   def portfolio_params
   params.require(:portfolio).permit(:title, 
                                     :subtitle, 
                                     :body, 
                                     technologies_attributes: [:name])
+  end
+
+
+  def set_portfolio_item
+   @portfolio_item = Portfolio.find(params[:id])
   end
 
 end
