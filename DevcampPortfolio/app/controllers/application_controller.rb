4f8fefd739a2339e18e57a26c07d2379f9e1b50c
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 16:49:51 +0000

    Installed and used gem

--- DevcampPortfolio/app/controllers/application_controller.rb
+++ DevcampPortfolio/app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
   include CurrentUser
   include DefaultPageConcern
   
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = OlearyViewTool::Renderer.copyright 'Connor O\'Leary', 'All rights reserved'
+  end
 end
+
